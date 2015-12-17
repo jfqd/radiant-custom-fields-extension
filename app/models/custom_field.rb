dce7e3e5724bf0017766d2202124e0dacb4ae2a8
jfqd <jfqd@blun.org>
2015-12-17 15:32:31 +0100

    a value is not required. If so would not be able to create a stub of fields for each page we need to.

--- app/models/custom_field.rb
+++ app/models/custom_field.rb
@@ -3,7 +3,7 @@ class CustomField < ActiveRecord::Base
 
   validates_presence_of       :name
   validates_uniqueness_of     :name, :scope => :page_id
-  validates_presence_of       :value
+  # validates_presence_of       :value
   validates_presence_of       :page_id
   validates_presence_of       :site_id, :if => :vhost_extension?
   validates_presence_of       :locale,  :if => :globalize2_extension?
