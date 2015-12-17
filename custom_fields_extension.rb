e07d24ce0328c0d1c0b731a95a470a43fa044d65
jfqd <jfqd@blun.org>
2015-12-17 12:08:21 +0100

    move partials into pages folder

--- custom_fields_extension.rb
+++ custom_fields_extension.rb
@@ -1,19 +1,20 @@
 # Uncomment this if you reference any of your controllers in activate
 require_dependency 'application_controller'
+require 'radiant-custom_fields-extension'
 
 class CustomFieldsExtension < Radiant::Extension
-  version "0.9" # this version is compatible with Radiant 0.9
-  description "An extension that lets you add custom fields to a Radiant page."
-  url "http://blog.aissac.ro/radiant/custom-fields-extension/"
+  version     RadiantCustomFieldsExtension::VERSION
+  description RadiantCustomFieldsExtension::DESCRIPTION
+  url         RadiantCustomFieldsExtension::URL
   
   def activate
     Page.send(:include, CustomFields::PageExtensions)
     Page.send(:include, CustomFields::CustomFieldsTags)
     
-    admin.page.edit.add :main, "admin/custom_fields/show_custom_fields", :before => "edit_header"
-    admin.page.edit.add :popups, "admin/custom_fields/custom_fields_popup"
+    admin.page.edit.add :part_controls, "show_custom_fields"
+    admin.page.edit.add :main, "custom_fields_popup", :after => "edit_buttons"
   end
   
   def deactivate
-  end  
+  end
 end
