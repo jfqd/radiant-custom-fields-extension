18b542e8846e8cdb4880b4174dd8361cc8bff29b
jfqd <jfqd@blun.org>
2015-12-17 12:10:05 +0100

    fix validation for site_id and locale

--- app/models/custom_field.rb
+++ app/models/custom_field.rb
@@ -4,14 +4,24 @@ class CustomField < ActiveRecord::Base
   validates_presence_of       :name
   validates_uniqueness_of     :name, :scope => :page_id
   validates_presence_of       :value
-  validates_presence_of       :field_type
   validates_presence_of       :page_id
-  validates_presence_of       :site_id, :if => defined?(VhostExtension)
-  validates_presence_of       :locale,  :if => defined?(Globalize2Extension)
+  validates_presence_of       :site_id, :if => :vhost_extension?
+  validates_presence_of       :locale,  :if => :globalize2_extension?
   
   def self.find_assignable_custom_fields(page_id)
     all = find(:all, :group => "name").map(&:name)
     assigned = find(:all, :conditions => {:page_id => page_id}, :group => "name").map(&:name)
     all - assigned
   end
+  
+  private
+  
+  def vhost_extension?
+    defined?(VhostExtension)
+  end
+  
+  def globalize2_extension?
+    defined?(Globalize2Extension)
+  end
+
 end
