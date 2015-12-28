3dd48c296862e504fcc4e37b574134c005ed9806
jfqd <jfqd@blun.org>
2015-12-28 16:04:56 +0100

    make sure fields get destroy if page vanished

--- lib/custom_fields/page_extensions.rb
+++ lib/custom_fields/page_extensions.rb
@@ -1,9 +1,9 @@
 module CustomFields
-  module PageExtensions    
+  module PageExtensions
     def self.included(base)
       base.class_eval do
-        has_many :custom_fields
+        has_many :custom_fields, :dependent => :destroy
       end
-    end  
+    end
   end
 end
