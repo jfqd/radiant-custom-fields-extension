0f042ca2ce571efb82e25d7a7c59f440aa3a2012
jfqd <jfqd@blun.org>
2019-01-07 14:30:51 +0100

    bump version to 0.9.7

--- lib/radiant-custom_fields-extension.rb
+++ lib/radiant-custom_fields-extension.rb
@@ -1,5 +1,5 @@
 module RadiantCustomFieldsExtension
-  VERSION     = "0.9.6"
+  VERSION     = "0.9.7"
   SUMMARY     = "Custom fields for Radiant pages"
   DESCRIPTION = "An extension that allows you to add custom fields to Radiant CMS pages."
   URL         = "https://github.com/Aissac/radiant-custom-fields-extension"
